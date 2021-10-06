9796f760f3272273ca17684246943a31322f621b
j10ccc <blyb1739@gmail.com>
2021-10-06 17:36:24 +0800

    beautify

--- src/js/manage.js
+++ src/js/manage.js
@@ -11,18 +11,12 @@ function getMyConfess(userName) {
         url: "http://localhost:8080/manage",
         data: "user=" + userName, // GET请求发送字符串
         success: function(data) {
-            /* var box1 = $(".sheet");
-            var box2 = $(".object");
-            for (var i = 1; i <= 9; i++){ //用户自己发表的表白要 pin 在墙头
-                box1[i - 1].innerHTML = data.content[i];
-                box2[i - 1].innerHTML = "—— " + data.tidyName[i];
-                
-            } */
+            console.log(data.content.length);
             for (i = 0; i < data.content.length; i++){
-                
-                //console.log(data.content[i] + " " + data.tidyName[i] + " " + data.id[i]); 
-                var ele = "<div class='note'><span class='quote'>“</span><span id='sheet'>" + data.content[i] + "</span><div class='attach'><span class='check'>No." + data.id[i] + "</span><span class='object'>—— " + data.tidyName[i] + "</span></div><div id='tools'><button id='edit'>编辑</button><button id='delect'>删除</button></div></div>";
-                $("#internal").append(ele);
+                var ele = document.createElement("div");
+                ele.className = "note";
+                ele.innerHTML = "<span class='quote'>“</span><span id='sheet'>" + data.content[i] + "</span><div class='attach'><span class='check'>No." + data.id[i] + "</span><span class='object'>—— " + data.tidyName[i] + "</span></div><div id='tools'><button id='edit'>编辑</button><button id='delect'>删除</button></div>";
+                document.getElementsByClassName("column")[i % 3].appendChild(ele); 
             }
         },
         error: function(jqXHR) {console.log("Error:" + jqXHR.status);}
